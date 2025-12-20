696c20e1f0a1d9f3b53f5bbbdd70447c758e027d
Jason Li <jligh145@gmail.com>
2025-12-19 23:15:31 -0500

    Add load spinner while sending and disabling multiple sends

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,10 +19,14 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = "";
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
       final url = Uri.https(
         'shopping-list-d535d-default-rtdb.firebaseio.com',
         'shopping-list.json',
@@ -137,14 +141,18 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
+                    onPressed:_isSending ? null: () {
                       _formKey.currentState!.reset();
                     },
                     child: const Text("Reset"),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,
-                    child: const Text("Add Item"),
+                    onPressed: _isSending ? null: _saveItem,
+                    child: _isSending ? SizedBox(
+                      height:16,
+                      width:16,
+                      child:  CircularProgressIndicator(),
+                    ): Text("Add Item"),
                   ),
                 ],
               ),
